8e0f90fb15e8968d639b1c145adc0b3706f5400b
Vlad Hreben <rct.grebenva@outlook.com>
2018-02-19 23:55:07 +0300

    winston

--- server/server.js
+++ server/server.js
@@ -1,19 +1,10 @@
 var http = require("http");
-var url = require("url");
+var log = require("winston");
 
-var server = new http.Server(function (req, res) {
-    console.log(req.method, req.url);
-    var urlParse = url.parse(req.url, true);
-    console.log(urlParse);
-    if(req.method == "GET" && urlParse.pathname == "/echo" && urlParse.query.message){
-        res.setHeader("Cache-controle", "no cache");
-        res.end(urlParse.query.message);
-    }else{
-        res.statusCode = 404;
-        res.end("Page not found");
-    }
-});
+var server = new http.Server();
 
+server.on("request", require("./request"));
 
 server.listen(1337, "127.0.0.1");
- 
+
+log.info("Server is running");
