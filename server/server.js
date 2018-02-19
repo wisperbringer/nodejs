547cd5b946ccaa782e024a27cc9424175fd1349c
Vlad Hreben <rct.grebenva@outlook.com>
2018-02-19 17:09:38 +0300

    simple echo server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 var http = require("http");
+var url = require("url");
 
-var server = new http.Server();
+var server = new http.Server(function (req, res) {
+    console.log(req.method, req.url);
+    var urlParse = url.parse(req.url, true);
+    console.log(urlParse);
+    if(urlParse.pathname == "/echo" && urlParse.query.message){
+         res.end(urlParse.query.message);
+    }else{
+        res.statusCode = 404;
+        res.end("Page not found");
+    }
+});
 
 server.listen(1337, "127.0.0.1");
-var counter = 0;
-server.on("request", function (req, res) {
-    res.end("Hello, world" + ++counter);
-});
