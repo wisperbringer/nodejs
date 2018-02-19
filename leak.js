51216eade07a38e4050e98bd8bc59a9156560c33
Vlad Hreben <rct.grebenva@outlook.com>
2018-02-19 16:18:34 +0300

    eventEmitter
    Проблема с setMaxListener(0)

--- leak.js
+++ leak.js
@@ -5,18 +5,25 @@ var db = new EventEmitter();
 function Request() {
     var self= this;
 
-    this.baiData = new Array("1e6").join("*");
+    this.baiData = new Array(1e6).join("*");
 
     this.send = function (data) {
         console.log(data);
-    }
+    };
 
-    db.on("data", function (info) {
+    function onData(info) {
         self.send(info);
-    })
+    }
+    
+    this.end = function () {
+        db.removeListener("data", onData);
+    };
+
+    db.on("data", onData);
 }
 
 setInterval(function () {
     var request = new Request();
+    request.end();
     console.log(process.memoryUsage().heapUsed);
 }, 200);
