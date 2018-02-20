b8377a521ba406b569c218ac4f67783a18149b88
Vlad Hreben <rct.grebenva@outlook.com>
2018-02-20 10:20:21 +0300

    server changed from sync to async

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 var http = require("http");
-var log = require("./log")(module);
+var fs = require("fs");
 
-var server = new http.Server();
-
-server.on("request", require("./request"));
-
-server.listen(1337, "127.0.0.1");
-
-log.info("Server is running");
+http.createServer(function (req, res) {
+    var info;
+    if(req.url == "/"){
+        fs.readFile("index.html", function (err, info) {
+            if(err){
+                console.error(err);
+                res.statusCode = 500;
+                res.end("An error occurred on the server");
+                return;
+            }
+            res.end(info);
+        })
+    }
+}).listen(3000);
